bff62aed00a862bd6b1b6ff60148c83d6cb6932b
sowm04 <sowman04@gmail.com>
2019-05-08 20:31:15 -0700

    first commit

--- main.php
+++ main.php
@@ -3,12 +3,12 @@
 
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: ind.html');
+  	header('location: index.html');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
   	unset($_SESSION['username']);
-  	header("location: ind.html");
+  	header("location: index.html");
   }
 ?>
 <!DOCTYPE html>
